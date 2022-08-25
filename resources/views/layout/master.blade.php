ded06e3421a1d5d6e59a61deb3fd85fff61dff13
sabaijung <ardsri.s@gmail.com>
2022-08-25 16:23:47 +0700

    เพิ่มไฟล์ layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 	<meta name="author" content="">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>MIS REPORT</title>
+	<!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+	<script src="https://kit.fontawesome.com/e60fae2e49.js" crossorigin="anonymous"></script>
 	@include('partials.styles')
 </head>
 
